451b652744ed17ce496c8cfaded6336e4abb7e10
s.sanchez <s.sanchez@iinext.fr>
2020-11-27 13:23:02 +0100

    create specif method in repository that return paginator, and add representation class then add annotation in controllers #11

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -19,32 +20,16 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function listAll($order = 'asc', $limit = 5, $offset = 1, $customer)
     {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+        $qb = $this->createQueryBuilder('u')
+            ->join('u.customer', 'c')
+            ->where('c.id = :customer')
+            ->setParameter('customer', $customer->getId())
+            ->setFirstResult(($offset - 1) * $limit)
+            ->setMaxResults($limit)
+            ->orderBy('u.lastname', $order);
 
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return new paginator($qb);
     }
-    */
 }
