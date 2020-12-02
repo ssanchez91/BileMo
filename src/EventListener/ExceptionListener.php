03e4491be126142f034793031d6dd08f9357be91
s.sanchez <s.sanchez@iinext.fr>
2020-12-02 15:59:27 +0100

    spilt code in 2 methods and replace if else by switch case #39

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
+use OpenApi\Annotations\Response as AnnotationsResponse;
+use SebastianBergmann\CodeCoverage\Report\Xml\Report;
 
 class ExceptionListener
 {
@@ -20,27 +22,35 @@ class ExceptionListener
         // Customize your response object to display the exception details
         $response = new Response();
         $response->setContent($message);
-
-        // HttpExceptionInterface is a special type of exception that
-        // holds status code and header details
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
-            $response->setContent(json_encode(['code' => $exception->getStatusCode(), 'message' => $exception->getMessage()]));
-        } else if ($exception instanceof JWTDecodeFailureException) {
-            $response->setContent(json_encode(['code' => 401, 'message' => $exception->getMessage()]));
-            $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
-        } else if ($exception instanceof ForbiddenException) {
-            $response->setContent(json_encode(['code' => 403, 'message' => $exception->getMessage()]));
-            $response->setStatusCode(Response::HTTP_FORBIDDEN);
-        } else if ($exception instanceof ResourceNoValidateException) {
-            $response->setContent(json_encode(['code' => 404, 'message' => $exception->getMessage()]));
-            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
-        } else {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+        $response = $this->prepareNewResponse($response, $exception, $message);        
 
         // sends the modified response object to the event
         $event->setResponse($response);
+    }
+
+    public function prepareNewResponse(Response $response, \Exception $exception, $message):Response
+    {
+        switch(true) {
+            case $exception instanceof HttpExceptionInterface:
+                $response->setStatusCode($exception->getStatusCode());
+                $response->headers->replace($exception->getHeaders());
+                $response->setContent(json_encode(['code' => $exception->getStatusCode(), 'message' => $message]));
+                break;
+            case $exception instanceof JWTDecodeFailureException:
+                $response->setContent(json_encode(['code' => 401, 'message' => $message]));
+                $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
+                break;
+            case $exception instanceof ForbiddenException:
+                $response->setContent(json_encode(['code' => 403, 'message' => $message]));
+                $response->setStatusCode(Response::HTTP_FORBIDDEN);
+                break;
+            case $exception instanceof ResourceNoValidateException:
+                $response->setContent(json_encode(['code' => 404, 'message' => $message]));
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+                break;
+            default:
+                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);            
+        }
+        return $response;
     }
 }
